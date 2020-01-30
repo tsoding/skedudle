56951119daecf7ac2f7e0c37881a31af7f137f9c
rexim <reximkut@gmail.com>
2020-01-30 20:08:21 +0700

    Add TODO(#21)

--- src/json.c
+++ src/json.c
@@ -65,7 +65,7 @@ static Json_Result parse_json_array(Memory *memory, String source)
 
 static Json_Result parse_json_object(Memory *memory, String source)
 {
-    assert(!"TODO: parse_json_object is not implemented");
+    assert(!"TODO(#21): parse_json_object is not implemented");
     return json_error;
 }
 
